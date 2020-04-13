4a1a657535cc33b583e7c48a93209ce62abb1a60
James Petersen <jpetersenames@gmail.com>
2020-04-13 13:03:43 -0400

    added testing table

--- webserver_test.go
+++ webserver_test.go
@@ -3,29 +3,21 @@ package main
 import "testing"
 
 func TestIncrement(t *testing.T) {
-    num := 0
-    want := num + 1
-    got := Increment(&num)
-    if got != want {
-        t.Errorf("Increment incorrect. Got: %d, want: %d.", got, want)
+
+    tables := []struct {
+        num  int
+        want int
+    }{
+        {1, 2},
+        {2, 3},
+        {10, 11},
+        {31, 32},
+    }
+
+    for _, table := range tables {
+        got := Increment(&table.num)
+        if got != table.want {
+            t.Errorf("Incrementing (%d) incorrect. Got: %d, want: %d.", table.num, got, table.want)
+        }
     }
 }
-// func TestIncrement(t *testing.T) {
-//
-//     tables := []struct {
-//         num  int
-//         want int
-//     }{
-//         {1, 2},
-//         {2, 3},
-//         {10, 11},
-//         {31, 32},
-//     }
-//
-//     for _, table := range tables {
-//         got := Increment(&table.num)
-//         if got != table.want {
-//             t.Errorf("Incrementing (%d) incorrect. Got: %d, want: %d.", table.num, got, table.want)
-//         }
-//     }
-// }
